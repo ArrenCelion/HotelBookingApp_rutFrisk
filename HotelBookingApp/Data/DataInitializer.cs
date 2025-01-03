e537a58145e423a21df4e5efe8c417f4266fb6f0
ArrenCelion <rut.nslund@gmail.com>
2025-01-03 21:53:00 +0100

    final final cleanup and fixes

--- HotelBookingApp/Data/DataInitializer.cs
+++ HotelBookingApp/Data/DataInitializer.cs
@@ -85,67 +85,10 @@ namespace HotelBookingApp.Data
                     .RuleFor(g => g.Address, f => f.Address.StreetAddress())
                     .RuleFor(g => g.PostalCode, f => f.Address.ZipCode())
                     .RuleFor(g => g.City, f => f.Address.City())
-                    .RuleFor(g => g.IsActive, f => f.Random.Bool(0.1f));
+                    .RuleFor(g => g.IsActive, f => f.Random.Bool(0.9f));
 
                 guestFaker.Generate(10).ForEach(g => _dbContext.Add(g));
             }
         }
-
-
-        /* private void SeedMenus(ApplicationDbContext dbContext)
-         {
-             if (!dbContext.Menu.Any(m => m.Name == "Main Menu"))
-             {
-                 dbContext.Menu.Add(new Menu
-                 {
-                     MenuName = "Main Menu",
-                     MenuPrompt = "Welcome to the Hotell, what do you wanna check?",
-                     //MenuOptions = { "Rooms", "Guests", "Bookings", "Close Application" } //Göra till ett eget table i en class som heter MenuOptions som blir seedad? och lägga in MenuId som fk i den?
-                 });
-             }
-
-             if (!dbContext.Menu.Any(m => m.Name == "Room Menu"))
-             {
-                 dbContext.Menu.Add(new Menu
-                 {
-                     MenuName = "Room Menu",
-                     MenuPrompt = "Room Menu:",
-                     //MenuOptions = { "Rooms", "Guests", "Bookings", "Close Application" } //Göra till ett eget table i en class som heter MenuOptions som blir seedad? och lägga in MenuId som fk i den?
-                 });
-             }
-
-             if (!dbContext.Menu.Any(m => m.Name == "Guest Menu"))
-             {
-                 dbContext.Menu.Add(new Menu
-                 {
-                     MenuName = "Guest Menu",
-                     MenuPrompt = "Guest Menu:",
-                     //MenuOptions = { "Rooms", "Guests", "Bookings", "Close Application" } //Göra till ett eget table i en class som heter MenuOptions som blir seedad? och lägga in MenuId som fk i den?
-                 });
-             }
-
-             if (!dbContext.Menu.Any(m => m.Name == "Reservation Menu"))
-             {
-                 dbContext.Menu.Add(new Menu
-                 {
-                     MenuName = "Reservation Menu",
-                     MenuPrompt = "Reservation Menu:",
-                     //MenuOptions = { "Rooms", "Guests", "Bookings", "Close Application" } //Göra till ett eget table i en class som heter MenuOptions som blir seedad? och lägga in MenuId som fk i den?
-                 });
-             }
-
-         }
-
-         private void SeedMenuOptions(ApplicationDbContext dbContext)
-         {
-             if (!dbContext.MenuOption.Any(m => m.Name == "Main Menu"))
-             {
-                 dbContext.MenuOption.Add(new MenuOption
-                 {
-                     OptionName = "Main Menu",
-                     MenuId = 0,
-                 });
-             }
-         }*/
     }
 }
